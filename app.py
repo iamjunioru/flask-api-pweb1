9bbbc99c29012bf4a1759011b0a1e764fec964e2
Junior <iamjunioru@gmail.com>
2023-09-05 16:19:16 -0300

    feat: /auth route

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api
 from models import db, ma
 from resources import TutorResource, PetResource, GetAllResource
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from models import Tutor
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['JWT_SECRET_KEY'] = 'secret'
+jwt = JWTManager(app)
+
 api = Api(app)
 db.init_app(app)
 ma.init_app(app)
@@ -15,6 +20,19 @@ with app.app_context():
 @app.route("/")
 def welcome():
     return "Welcome to the Tutor and Pet Management API!"
+
+@app.route("/auth", methods=["POST"])
+def authenticate():
+    data = request.get_json()
+    email = data.get("email")
+    senha = data.get("senha")
+
+    user = Tutor.query.filter_by(email=email).first()
+    if user and user.check_senha(senha):
+        access_token = create_access_token(identity=user.id)
+        return {"access_token": access_token}, 200
+    else:
+        return {"message": "email or pass invalid"}, 401
 
 api.add_resource(GetAllResource, "/all") 
 api.add_resource(TutorResource, "/tutor", "/tutor/<int:tutor_id>")
